e635fc0f2a98c5d9fd365306d9513690bebf959c
tatsuya-kusumoto <164455573+tatsuya-kusumoto@users.noreply.github.com>
2024-07-06 15:58:15 +0900

    Update startup.sh

--- web/startup.sh
+++ web/startup.sh
@@ -2,7 +2,7 @@
 
 # Spring Bootアプリケーションの起動
 ↓のjarファイル名を修正
-java -jar /var/www/java/KadaiFirstSpringBoot-0.0.1-SNAPSHOT.jar &
+java -jar /var/www/java/DailyReportSystemApplication-0.0.1-SNAPSHOT.jar &
 
 # Apache2の起動
 rm -f /var/run/apache2/apache2.pid
